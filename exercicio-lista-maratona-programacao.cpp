3d9e528884c9d25b239d82bc14432aaca0cf35db
Leonardo Braz <lhleonardo@hotmail.com>
2019-02-23 21:26:22 -0300

    Finalizando ex 1533

--- exercicio-lista-maratona-programacao.cpp
+++ exercicio-lista-maratona-programacao.cpp
@@ -3,25 +3,26 @@
 using std::string;
 using std::cin;
 using std::cout;
+using std::ostream;
 
-template <class TipoElemento>
-class Noh {
-    friend class ListaGenerica;
+template <class tipo_elemento>
+class noh {
+    friend class lista_generica;
 
   private:
-    TipoElemento dado;
-    Noh *proximo;
+    tipo_elemento dado;
+    noh *proximo;
 };
 
 // lista com encadeamento simples
-template <class TipoElemento>
-class ListaGenerica {
+template <class tipo_elemento>
+class lista_generica {
   public:
-    ListaGenerica();
-    ~ListaGenerica();
+    lista_generica();
+    ~lista_generica();
 
     // adiciona um novo elemento na lista
-    void adiciona(TipoElemento valor);
+    void adiciona(tipo_elemento valor);
 
     // percorre a lista e devolve o elemento presente
     // o parâmetro *valorParaBusca* será comparado com todos os elementos
@@ -29,53 +30,56 @@ class ListaGenerica {
     // para que funcione corretamente.
     // Caso alguma comparação seja de sucesso, o método retornará True
     // e o elemento desejado estará presente no ponteiro *elementoExistente*
-    bool procura(TipoElemento valorParaBusca, TipoElemento *elementoExistente);
+    bool procura(tipo_elemento valorParaBusca, tipo_elemento *elementoExistente);
+
+    // deverá retornar nullptr se índice estiver fora do intervalo
+    tipo_elemento* acessa_posicao(unsigned indice);
 
     // tamanho da lista
     unsigned tamanho();
 
   private:
-    Noh<TipoElemento> *inicio;
-    Noh<TipoElemento> *fim;
+    noh<tipo_elemento> *inicio;
+    noh<tipo_elemento> *fim;
 
     unsigned qtdElementos;
 };
 
-template <class TipoElemento>
-ListaGenerica<TipoElemento>::ListaGenerica() {
+template <class tipo_elemento>
+lista_generica<tipo_elemento>::lista_generica() {
     // iniciar atributos
 }
 
-template <class TipoElemento>
-ListaGenerica<TipoElemento>::~ListaGenerica() {
+template <class tipo_elemento>
+lista_generica<tipo_elemento>::~lista_generica() {
     // desalocar valores em memória
 }
 
-template <class TipoElemento>
-void ListaGenerica<TipoElemento>::adiciona(TipoElemento valor) {
+template <class tipo_elemento>
+void lista_generica<tipo_elemento>::adiciona(tipo_elemento valor) {
     // adicionar elemento na lista
 }
 
-template <class TipoElemento>
-bool ListaGenerica<TipoElemento>::procura(TipoElemento valorParaBusca, TipoElemento *elementoExistente) {
+template <class tipo_elemento>
+bool lista_generica<tipo_elemento>::procura(tipo_elemento valorParaBusca, tipo_elemento *elementoExistente) {
     // procurar valorParaBusca na lista. Caso exista algum destes que atenda o critério
     // de igualdade, o mesmo deverá ser feito uma cópia e destinado para
     // elementoExistente.
 }
 
-template <class TipoElemento>
-unsigned ListaGenerica<TipoElemento>::tamanho() { return this->qtdElementos }
+template <class tipo_elemento>
+unsigned lista_generica<tipo_elemento>::tamanho() { return this->qtdElementos }
 
-class Integrante {
+class integrante {
   public:
-    Integrante(string nome, string nickname);
+    integrante(string nome, string nickname);
 
-    void defineComoLider();
+    void define_como_lider();
 
-    string getNome();
-    string getNickname();
+    string acessar_nome();
+    string acessar_nickname();
 
-    bool operator==(const Integrante& outro);
+    bool operator==(const integrante& outro);
 
   private:
     string nome;
@@ -84,79 +88,85 @@ class Integrante {
     bool lider;
 };
 
-Integrante::Integrante(string nome, string nickname) {
+integrante::integrante(string nome, string nickname) {
     // inicializar as variáveis
 }
 
-string Integrante::getNome() {
+string integrante::acessar_nome() {
     return this->nome;
 }
 
-string Integrante::getNickname() {
+string integrante::acessar_nickname() {
     return this->nickname;
 }
 
-void Integrante::defineComoLider() {
+void integrante::define_como_lider() {
     this->lider = true;
 }
 
-bool Integrante::operator==(const Integrante& outro) {
+bool integrante::operator==(const integrante& outro) {
     return this->lider == outro.lider == true;
 }
 
-class Equipe {
+class equipe {
   public:
-    Equipe(string nome, string linguagem);
-    ~Equipe();
+    equipe(string nome, string linguagem);
+    ~equipe();
 
-    string getNome();
-    string getLinguagem();
+    string acessar_nome();
+    string acessar_linguagem();
 
-    void adicionarIntegrante(Integrante novoMembro, bool definirComoLider);
+    void adicionar_integrante(integrante novoMembro, bool definirComoLider);
 
-    unsigned qtdIntegrantes();
+    unsigned qtd_integrantes();
+
+    void imprimir(ostream& saida);
   private:
     string nome;
     string linguagem;
 
     // uma lista de integrantes...
-    ListaGenerica<Integrante> *integrantes;
+    lista_generica<integrante> *integrantes;
 };
 
-Equipe::Equipe(string nome, string linguagem) {
+equipe::equipe(string nome, string linguagem) {
     // inicializar as variáveis...
 }
 
-Equipe::~Equipe() {
+equipe::~equipe() {
     // destruir alocações caso realizadas
 }
 
-string Equipe::getNome() {
+string equipe::acessar_nome() {
     return this->nome;
 }
 
-string Equipe::getLinguagem() {
+string equipe::acessar_linguagem() {
     return this->linguagem;
 }
 
+void equipe::imprimir(ostream& saida){
+    // ...
+}
+
 // deve verificar se já existe algum lider da equipe antes...
-void Equipe::adicionarIntegrante(Integrante novoMembro, bool isLider) {
-    if (isLider) novoMembro.defineComoLider();
+void equipe::adicionar_integrante(integrante novoMembro, bool isLider) {
+    if (isLider) novoMembro.define_como_lider();
 
     if (not this->integrantes->procura(novoMembro, NULL)) {
         this->integrantes->adiciona(novoMembro);
-        this->qtdIntegrantes++;
+        this->qtd_integrantes++;
     }
 }
 
-unsigned Equipe::qtdIntegrantes() {
+unsigned equipe::qtd_integrantes() {
     return this->integrantes->tamanho();
 }
 
 int main() {
     char leitura;
 
-    ListaGenerica<Equipe>* equipes = new ListaGenerica<Equipe>();
+    lista_generica<equipe>* equipes = new lista_generica<equipe>();
 
     do {
         cin >> leitura;
@@ -166,7 +176,8 @@ int main() {
             case 'i':
                 // fazer a leitura dos dados de uma equipe e inserir na lista
                 break;
-        
+            case 'p':
+                // deverá imprimir a lista
             default:
                 break;
         }
